99257f62fa42c2b9075deb08887e46c665cea9cb
Eduardo Damián Cabaña <edc.damian2@gmail.com>
2024-11-14 23:08:11 -0300

    "Modifique Cliente y Persona. Cree ClienteVip"

--- Cliente.ts
+++ Cliente.ts
@@ -1,12 +1,21 @@
-import { Persona } from "./Persona";
+import { Persona } from './Persona';
 
-export class Cliente extends Persona{
-    private 
-    
+export class Cliente extends Persona {
+    cantidadVisita: number;
+    clienteVIP: boolean;
 
-    constructor(nombre:string,apellido:string,dni:number,domicilio:string|number,numeroId:number){
-        super()
+    constructor(nombre: string, apellido: string, dni: number, domicilio: string, numeroId: number, cantidadVisita: number) {
+        super(nombre, apellido, dni, domicilio, numeroId, cantidadVisita);
+        this.cantidadVisita = 0;
+        this.clienteVIP = false;
+    }
 
-   
-}
-  
+    sumarVisita() {
+        this.cantidadVisita++;
+        if (this.cantidadVisita >= 5) {
+            this.clienteVIP = true;
+        }
+    }
+};
+
+
