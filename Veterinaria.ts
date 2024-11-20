dfebc35cdd9b5d5a4339fac8a8562c9b4dc2508b
Eduardo Damián Cabaña <edc.damian2@gmail.com>
2024-11-20 20:55:28 -0300

    Actualización archivo main,Clase Veterinaria y Calse  Paciente

--- Veterinaria.ts
+++ Veterinaria.ts
@@ -1,71 +1,69 @@
 import { stringify } from "querystring";
 import { Cliente } from "./Cliente";
 import { Persona } from "./Persona";
-import { Proveedor } from "./proveedor";
+import { Proveedor } from "./Proveedor";
 
-export class Veterinaria{
+export class Veterinaria {
     private nombre: string;
     private direccion: string;
-    private id:number;
-    private personas:Persona[]=[];
-    private clientes:Cliente[]=[];
-    private proveedores:Proveedor[]=[];
-    
-    constructor(nombre:string,direccion:string,id:number){
-        this.nombre=nombre;
-        this.direccion=direccion;
-        this.id=id;
+    private id: number;
+    private personas: Persona[] = [];
+    private clientes: Cliente[] = [];
+    private proveedores: Proveedor[] = [];
 
-       
-        
+    constructor(nombre: string, direccion: string, id: number) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.id = id;
+
     }
 
-    getNombre(){
+    getNombre() {
         return this.nombre;
     }
-    getDireccion(){
-       return this.direccion;
+    getDireccion() {
+        return this.direccion;
     }
-    getId(){
+    getId() {
         return this.id;
     }
-    setNombre(nombre:string){
-        this.nombre=nombre;
+    setNombre(nombre: string) {
+        this.nombre = nombre;
     }
-    setDireccion(direccion:string){
-        this.direccion=direccion;
+    setDireccion(direccion: string) {
+        this.direccion = direccion;
     }
-    setId(id:number){
-        this.id=id;
+    setId(id: number) {
+        this.id = id;
     }
-    getPaciente(){
+    getPaciente() {
         return this.personas;
     }
-    getCiente(){
+    getCiente() {
         return this.clientes;
     }
-    getConsulta(paciente:Persona):void{
+    getConsulta(paciente: Persona): void {
         this.personas.push(paciente);
         console.log(` paciente nuevo ${paciente.getNombre()} agregado`);
     }
-    agregarCliente(clientes:Cliente){
+    agregarCliente(clientes: Cliente) {
         this.clientes.push(clientes);
         console.log(` cliente ${Cliente.getNombre()} agregado`);
-    
-    }
-    eliminarCliente(clientes:Cliente){
-        this.clientes= this.clientes.filter(c => c.getNombre() !== clientes.getNombre());
-        console.log(`cliente selecionado ${clientes.getNombre()} ha sido eliminado`); 
 
     }
-    agregarProveedor(proveedores:Proveedor){
+    eliminarCliente(clientes: Cliente) {
+        this.clientes = this.clientes.filter(c => c.getNombre() !== clientes.getNombre());
+        console.log(`cliente selecionado ${clientes.getNombre()} ha sido eliminado`);
+
+    }
+    agregarProveedor(proveedores: Proveedor) {
         this.proveedores.push(proveedores);
         console.log(` proveedor ${Proveedor.getNombre()} agregado`);
-    
+
     }
-    eliminarProveedor(proveedores:Proveedor){
-        this.proveedores= this.proveedores.filter(p => p.getNombre() !== proveedores.getNombre());
-        console.log(`proveedor selecionado ${proveedores.getNombre()} ha sido eliminado`); 
+    eliminarProveedor(proveedores: Proveedor) {
+        this.proveedores = this.proveedores.filter(p => p.getNombre() !== proveedores.getNombre());
+        console.log(`proveedor selecionado ${proveedores.getNombre()} ha sido eliminado`);
 
     }
 }
