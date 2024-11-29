8df2efe92c698b1e876aba8b75523c25a8149569
Eduardo Damián Cabaña <edc.damian2@gmail.com>
2024-11-29 20:06:28 -0300

    Modificacion Cliente y clienteVip

--- Cliente.ts
+++ Cliente.ts
@@ -4,25 +4,25 @@ export class Cliente extends Persona {
     static getNombre() {
         throw new Error("Method not implemented.");
     }
-    clienteVip: boolean= false;
-    
+    clienteVip: boolean = false;
+
 
 
     constructor(nombre: string, apellido: string, dni: number, domicilio: string, numeroId: number, cantidadVisita: number) {
         super(nombre, apellido, dni, domicilio, numeroId);
         this.cantidadVisita = 0;
-        this.clienteVIP = false;
+        this.clienteVip = false;
     }
-    
-    sumarVisita(){
-         if(this.cantidadVisita >=5 ){
+
+    sumarVisita() {
+        if (this.cantidadVisita >= 5) {
 
             return this.clienteVip;
-         }
-         console.log("el cliente llego a 5 visitas ahora puede acceder a los beneficios");
-
-        
         }
+        console.log("el cliente llego a 5 visitas ahora puede acceder a los beneficios");
+
+
+    }
 };
 
 
