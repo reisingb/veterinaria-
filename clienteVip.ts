ee89802b9fb6174ffdcceaa0bd537485c0457a8a
Eduardo Damián Cabaña <edc.damian2@gmail.com>
2024-11-29 20:23:19 -0300

    Modificacion ClienteVip

--- clienteVip.ts
+++ clienteVip.ts
@@ -3,35 +3,37 @@ import { Cliente } from './Cliente';
 export class ClienteVIP extends Cliente {
     private beneficios: string[];
 
-    constructor(nombre: string, apellido: string, dni: number, domicilio: string, id: number, telefono: number) {
-        super(nombre, apellido, dni, domicilio, id, telefono);
+    constructor(nombre: string, apellido: string, dni: number, domicilio: string, numeroId: number, cantidadVisita: number) {
+        super(nombre, apellido, dni, domicilio, numeroId, cantidadVisita);
         this.beneficios = [
             "Descuento del 20% en compras y consultas",
             "Atención domiciliaria",
             "Servicio de peluquería",
         ];
     }
-
     obtenerBeneficios(): string[] {
         return this.beneficios;
     }
-
     agregarBeneficio(beneficio: string): void {
         if (!this.beneficios.includes(beneficio)) {
             this.beneficios.push(beneficio);
         }
     }
-
     eliminarBeneficio(beneficio: string): void {
-        this.beneficios = this.beneficios.filter(b => b !== beneficio);
+        this.beneficios = this.beneficios.filter((b) => b !== beneficio);
     }
-
     aplicarDescuento(monto: number): number {
         const descuento = 0.20;
         return monto - (monto * descuento);
     }
-
-    public mostrarResumen(): string {
-        return `Cliente VIP: ${this.nombre} ${this.apellido}, ID: ${this.getId()}, Dirección: ${this.domicilio}, Visitas: ${this.cantidadVisita}\n Beneficios: ${this.beneficios}`;
+    mostrarResumen(): string {
+        return `
+            Cliente VIP: ${this.getNombre} ${this.getApellido}
+            DNI: ${this.getDni}
+            Domicilio: ${this.getDomicilio}
+            Número ID: ${this.getId}
+            Cantidad de Visitas: ${this.cantidadVisita}
+            Beneficios Exclusivos: ${this.beneficios.join(", ")}
+        `;
     }
-}
+};
